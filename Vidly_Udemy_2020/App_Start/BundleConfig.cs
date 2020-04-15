66ce2ccfd0b897046ece41975f1bddb68234b0aa
gabee1987 Gabee <gabee1987@gmail.com>
2020-04-15 14:36:06 +0200

    Az első részt felfrissítettem

--- Vidly_Udemy_2020/App_Start/BundleConfig.cs
+++ Vidly_Udemy_2020/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly_Udemy_2020
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-paper.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Vidly_Udemy_2020/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly_Udemy_2020.Models;
+
+namespace Vidly_Udemy_2020.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Inception" };
+            return View( movie );
+        }
+    }
+}
